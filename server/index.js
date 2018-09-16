49cd1bf0010094248b009d38b3cda05271059495
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-17 02:52:10 +0300

    configuring passport and GoogleStrategy

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const passport = require('passport');
 // const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const {Strategy:GoogleStrategy} = require('passport-google-oauth20');
+const {Strategy: GoogleStrategy} = require('passport-google-oauth20');
+const keys = require('./config/keys');
+
 const app = express();
 
-// https://console.developers.google.com/apis/dashboard
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, accessToken => {
+    console.log('accessToken', accessToken);
+}));
 
-// clientID 601599414150-jgma83rtn6ntdr355il2cm1a6be0plqc.apps.googleusercontent.com
-// clientSecret NKOUcq5P6DjDlJuGmAJRqA7c
-
-passport.use( new GoogleStrategy());
-
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
