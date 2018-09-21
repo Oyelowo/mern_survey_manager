55b38b793d5b331f0b4d023f389552985685728c
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-21 22:25:41 +0300

    creating model and passing the id to the googleID in passport

--- server/models/User.js
+++ server/models/User.js
@@ -2,3 +2,5 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const userSchema = new Schema({googleID: String});
+
+mongoose.model('users', userSchema);
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,15 +1,14 @@
 const passport = require('passport');
 const {Strategy: GoogleStrategy} = require('passport-google-oauth20');
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-console.log('accessToken', accessToken);
-console.log('refreshToken', refreshToken);
-console.log('profile', profile);
+    new User({googleID: profile.id}).save()
 }));
-
