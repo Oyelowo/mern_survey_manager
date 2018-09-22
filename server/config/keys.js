f8c7de670a7b491553c19c9d6f3e2ecc01cdab9f
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-22 16:55:00 +0300

    auth flow finished

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,5 @@
 // figure out what set of credentials to return
-if (process.env.ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // we are in production, return production set of keys
     module.exports= require('./prod');
 } else {
