6947fb704fe713a8abfe7697b164dd3f0f7c90f9
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-23 09:49:13 +0300

    redux set up

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        
+        <h1>This is Oyelowo</h1>
       </div>
     );
   };
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {createStore, applyMiddleware} from "redux";
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+
+const store = createStore(() => [], {}, applyMiddleware());
+ReactDOM.render(
+    <Provider store={store}><App/></Provider>, document.querySelector('#root'));
