ec3f91d04d1dc1a166d97dd891c7fda082d640bc
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-23 11:23:12 +0300

    getting started with materialize css

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -3,7 +3,16 @@ import React, {Component} from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>Header</div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="left brand-logo">Survey Manager</a>
+                    <ul className="right">
+                        <li>
+                            <a>Signin With Google</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
