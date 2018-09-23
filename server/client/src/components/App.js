4d9eeda2c90430af39bbadffc4544df021ae377d
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-23 10:13:25 +0300

    using react router

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>This is Oyelowo</h1>
-      </div>
-    );
-  };
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    };
 };
 
 export default App;
---
+++ server/client/src/reducers/authReducer.js
@@ -0,0 +1,8 @@
+let initialtate = {}
+
+export default(state = initialtate, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
