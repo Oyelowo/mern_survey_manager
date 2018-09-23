68abc941436f243875d28f9b08cb47eab15c6503
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-23 14:25:55 +0300

    link used instead of anchor tag for handling home route

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -14,18 +15,23 @@ class Header extends Component {
                 )
 
             default:
-                return (<li>
-                    <a href="/api/logout">Logout</a>
-                </li>);
+                return (
+                    <li>
+                        <a href="/api/logout">Logout</a>
+                    </li>
+                );
         }
     }
 
     render() {
-        console.log(this.props);
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo">Survey Manager</a>
+                    <Link
+                        to={this.props.auth
+                        ? '/surveys'
+                        : '/'}
+                        className="left brand-logo">Survey Manager</Link>
                     <ul className="right">
                         {this.renderContent()}
 
---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{
+            textAlign: 'center'
+        }}>
+            <h1>Survey Manager!</h1>
+            <p>Get feedbacks from Users</p>
+        </div>
+    );
+};
+
+export default Landing;
