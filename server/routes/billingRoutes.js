898a9891e90437dd5c03be17a94787fdc2ea010d
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-25 21:13:10 +0300

    getting the token from stripe

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -4,6 +4,12 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
     app.post('/api/stripe', (req, res) => {
+        stripe.charges.create({
+            amount: 1000,
+            currency: 'usd',
+            description: '$10 for 10 credits',
+            source: res.body.id
+        });
         
     });
 };
