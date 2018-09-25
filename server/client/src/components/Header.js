98749f9fc88db245cc77ed70efaf0592f6c274cc
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-25 19:23:16 +0300

    getting started with react stripe checkout

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
     renderContent() {
@@ -8,17 +9,19 @@ class Header extends Component {
             case null:
                 return;
             case false:
-                return (
+                return 
                     <li>
                         <a href="/auth/google">Signin With Google</a>
                     </li>
-                )
+                
 
             default:
                 return (
+                    [
+                        <li><Payments /></li>,
                     <li>
                         <a href="/api/logout">Logout</a>
-                    </li>
+                    </li>]
                 );
         }
     }
