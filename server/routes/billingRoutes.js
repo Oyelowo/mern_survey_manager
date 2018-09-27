5eb342e4fb87c8461c74759ad4c26279e63a0b2c
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-27 23:32:32 +0300

    route specific middlewares

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,12 +1,10 @@
 const keys = require('../config/keys');
 
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-    app.post('/api/stripe', async(req, res) => {
-        if (!req.user) {
-            return res.status(401).send({error: 'You are not logged in'})
-        }
+    app.post('/api/stripe', requireLogin,  async(req, res) => {
         const charge = await stripe
             .charges
             .create({amount: 1000, currency: 'usd', description: '$10 for 10 credits', source: req.body.id});
