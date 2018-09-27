09d0f5c07940d3ddfb1dd153384ac8482427d4a3
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-27 23:17:40 +0300

    naive way of dealing with users that are not logged in in the api/stripe route

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,20 +2,20 @@ const keys = require('../config/keys');
 
 const stripe = require('stripe')(keys.stripeSecretKey);
 
-
 module.exports = app => {
-    app.post('/api/stripe', async (req, res) => {
-      const charge = await stripe.charges.create({
-            amount: 1000,
-            currency: 'usd',
-            description: '$10 for 10 credits',
-            source: req.body.id
-        });
+    app.post('/api/stripe', async(req, res) => {
+        if (!req.user) {
+            return res.status(401).send({error: 'You are not logged in'})
+        }
+        const charge = await stripe
+            .charges
+            .create({amount: 1000, currency: 'usd', description: '$10 for 10 credits', source: req.body.id});
 
         req.user.credits += 5;
-        const user =  await req.user.save();
+        const user = await req
+            .user
+            .save();
         res.send(user);
-        
-        
+
     });
 };
