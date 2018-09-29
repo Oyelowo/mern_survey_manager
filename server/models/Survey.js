18034d28518f4dbc819691da88b08123d3dc28c1
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-09-29 19:44:08 +0300

    survey model

---
+++ server/models/Survey.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const surveySchema = new Schema({
+    title: String,
+    subject: String,
+    body: String,
+    recipients: [String],
+    yes: Number,
+    no: Number
+});
+mongoose.model('surveys', surveySchema);
