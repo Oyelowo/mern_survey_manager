361ccf78b9f176976b23094cbe581702042e6cec
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-10-01 19:01:29 +0300

    Using the Mailer

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -21,6 +23,7 @@ module.exports = app => {
             dateSent: Date.now()
         });
 
-        await survey.save();
+        // Great place to send an email
+        const mailer = new Mailer(survey, surveyTemplate(survey));
     });
 };
---
+++ server/services/Mailer.js
@@ -0,0 +1,11 @@
+const sendgrid = require('sendgrid');
+const {mail: helper} = sendgrid;
+const keys = require('../config/keys');
+
+
+class Mailer extends helper.Mail {
+ 
+}
+
+
+module.exports ={Mailer}
