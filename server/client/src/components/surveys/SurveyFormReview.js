e0fa39eb4c22498e56b34815137e36b5914ea662
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-10-03 22:43:34 +0300

    send survey button

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,34 +1,34 @@
 // show users their inputs to review
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import formFields from './formFields';
 
+const SurveyFormReview = ({ onCancel, formValues }) => {
+	const reviewFields = formFields.map(({ label, name }) => (
+		<div key={name}>
+			<label>{label}</label>
+			{formValues[name]}
+		</div>
+	));
+	return (
+		<div>
+			<h5>Kindly confirm your input</h5>
+			<div>{reviewFields}</div>
+			<button className="yellow darken-3 white-text btn-flat" onClick={onCancel}>
+				Back
+			</button>
+			<button className="green btn-flat right">
+				Send Survey
+				<i className="material-icons right white-text">email</i>
+			</button>
+		</div>
+	);
+};
 
-
-const SurveyFormReview = ({onCancel, formValues}) => {
-    const reviewFields = formFields.map(({label, name})=> 
-    ( <div key={name}>
-         <label>{label}</label>
-         {formValues[name]}
-     </div>)
-     );
-    return (
-        <div>
-        <h5>Kindly confirm your input</h5>
-        <div>
-            
-            {reviewFields}
-        </div>
-     < button className = "yellow darken-3 btn-flat" onClick = {onCancel}>Back </button>
-     </div>
-     );
-}
-
-
-const mapStateToProps=(state)=>{
-    return{
-        formValues: state.form.surveyForm.values
-    };
-}
+const mapStateToProps = state => {
+	return {
+		formValues: state.form.surveyForm.values,
+	};
+};
 
 export default connect(mapStateToProps)(SurveyFormReview);
