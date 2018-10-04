875657f8a91dc1936cab736f2e95622b3e5b45e2
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-10-04 20:44:54 +0300

    unique events

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,3 +1,7 @@
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
+
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -7,41 +11,50 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
-        res.send('Thanks for voting')
-    })
+	app.get('/api/surveys/thanks', (req, res) => {
+		res.send('Thanks for voting');
+	});
 
-    app.post('/api/surveys', requireLogin, requireCredits, async(req, res) => {
-        const {title, subject, body, recipients} = req.body;
-        const survey = new Survey({
-            title,
-            subject,
-            body,
-            recipients: recipients
-                .split(',')
-                .map(email => ({
-                    email: email.trim()
-                })),
-            _user: req.user.id,
-            dateSent: Date.now()
-        });
+	app.post('/api/surveys/webhooks', (req, res) => {
+		const events = req.body.map(({ url, email }) => {
+			const pathname = new URL(url).pathname;
+			const p = new Path('/api/surveys/:surveyId/:choice');
+			const match = p.test(pathname);
+			if (match) {
+				return { email, surveyId: match.surveyId, choice: match.choice };
+			}
+		});
+		const compactEvents = _.compact(events);
+		const uniqueEvents = _.unionBy(compactEvents, 'email', 'surveyId');
+		console.log('uniqueEvents', uniqueEvents);
+		res.send({});
+		
+	});
 
-        // Great place to send an email
-        const mailer = new Mailer(survey, surveyTemplate(survey));
+	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+		const { title, subject, body, recipients } = req.body;
+		const survey = new Survey({
+			title,
+			subject,
+			body,
+			recipients: recipients.split(',').map(email => ({
+				email: email.trim(),
+			})),
+			_user: req.user.id,
+			dateSent: Date.now(),
+		});
 
-        try {
-            await mailer.send();
-            await survey.save();
-            req.user.credits -= 1;
-            const user = await req
-                .user
-                .save();
-            res.send(user);
-        } catch (error) {
-            res
-                .status(422)
-                .send(error);
-        }
+		// Great place to send an email
+		const mailer = new Mailer(survey, surveyTemplate(survey));
 
-    });
+		try {
+			await mailer.send();
+			await survey.save();
+			req.user.credits -= 1;
+			const user = await req.user.save();
+			res.send(user);
+		} catch (error) {
+			res.status(422).send(error);
+		}
+	});
 };
