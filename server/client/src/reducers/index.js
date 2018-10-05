532bca230b81143273db9c50980fe42b88e4b8d5
Oyelowo <31687368+Oyelowo@users.noreply.github.com>
2018-10-05 21:03:10 +0300

    rendering list of surveys

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -1,5 +1,6 @@
-import {combineReducers} from 'redux';
-import authReducer from "./authReducer";
-import {reducer as reduxForm} from 'redux-form';
+import { combineReducers } from 'redux';
+import authReducer from './authReducer';
+import { reducer as reduxForm } from 'redux-form';
+import surveysReducer from './surveysReducer';
 
-export default combineReducers({auth: authReducer, form: reduxForm});
+export default combineReducers({ auth: authReducer, form: reduxForm, surveys: surveysReducer });
